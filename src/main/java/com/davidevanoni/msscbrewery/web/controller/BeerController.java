320e9bae21b1cfce9f6ea13e43871443b9cfea3e
Davide Vanoni <davide.vanoni@gmail.com>
2020-05-13 14:43:01 +0200

    Added save method to Beer object and relative POST method endpoint

--- src/main/java/com/davidevanoni/msscbrewery/web/controller/BeerController.java
+++ src/main/java/com/davidevanoni/msscbrewery/web/controller/BeerController.java
@@ -2,12 +2,10 @@ package com.davidevanoni.msscbrewery.web.controller;
 
 import com.davidevanoni.msscbrewery.web.model.BeerDto;
 import com.davidevanoni.msscbrewery.web.service.BeerService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -24,5 +22,15 @@ public class BeerController {
     @GetMapping({"/{beerId}"})
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping // POST - create new beer
+    public ResponseEntity handlePost(BeerDto beerDto){
+        BeerDto saveDto = beerService.saveNewBeer(beerDto);
+        HttpHeaders headers = new HttpHeaders();
+        //This is by convention each time we call POST method to create an object: include Location header with the ID of the object created.
+        //TODO add hostname to URL
+        headers.add("Location", "/api/v1/beer" + saveDto.getId().toString());
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 }
