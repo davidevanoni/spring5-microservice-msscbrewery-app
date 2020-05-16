acd5788c5641edaf83f31d732c223ac023cc174d
Davide Vanoni <davide.vanoni@gmail.com>
2020-05-16 12:37:04 +0200

    Added a controller advice class in order to manage globally ConstraintViolationException and BindException

--- src/main/java/com/davidevanoni/msscbrewery/web/controller/BeerController.java
+++ src/main/java/com/davidevanoni/msscbrewery/web/controller/BeerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.UUID;
 
-//@Deprecated
+@Deprecated
 @RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
